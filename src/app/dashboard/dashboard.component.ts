9161156fddf2cf8e8eec5a59539e3662d33f7d89
David Hamilton <davidhamilton473@gmail.com>
2023-12-05 17:14:54 -0500

    finished

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  constructor(private router: Router){}
+
   scrollTo(section: string): void {
     const element = document.getElementById(section);
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
     }
+  }
+  backToHome(){
+    this.router.navigate([''])
+  }
+  goToAboutMe(){
+    this.router.navigate(['about-me'])
   }
 }

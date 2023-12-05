15d761899a6f0fbd44cf9555838b52690bc9d213
David Hamilton <davidhamilton473@gmail.com>
2023-12-05 15:29:19 -0500

    wokring on it

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -11,5 +11,7 @@ constructor(private router: Router){}
 openLinkedIn(){
   window.open('https://www.linkedin.com/in/david-hamilton-277639217', '_blank');
 }
-
+openGithub(){
+  window.open('https://github.com/Hamilton8560', '_blank');
+}
 }
